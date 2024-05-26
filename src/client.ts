b7bbee6979c7acebbc9bc893c26043aae0daaee4
Thounyy <thouny@tuta.io>
2024-05-26 22:11:28 +0200

    wip: Kiosk

--- src/client.ts
+++ src/client.ts
@@ -344,5 +344,194 @@ export class KrakenClient {
 			],
 		});
 	}
+
+	// === Kiosk ===
+
+	async getKiosks(): Promise<Kiosk[]> {
+		let capIds: string[] = [];
+		let kioskIds: string[] = [];
+		let cursor = null;
+		let page = true;
+
+		while (page) {
+			const { data, nextCursor, hasNextPage } = await this.client.getOwnedObjects({
+				owner: this.multisigId,
+				filter: { StructType: `${FRAMEWORK}::kiosk::KioskOwnerCap` },
+				options: { showContent: true },
+				cursor
+			});
+
+			capIds = capIds.concat(data.map(cap => cap.data?.objectId!));
+			kioskIds = kioskIds.concat(data.map((cap: any) => cap.data?.content?.fields.for));
+			cursor = nextCursor;
+			page = hasNextPage;
+		}
+		
+		let kiosks: Kiosk[] = [];
+
+		for (let i = 0; i < kioskIds.length; i += 25) {
+			const ids = kioskIds.slice(i, i + 25);
+			const objects = await this.client.multiGetObjects({
+				ids,
+				options: { showContent: true }
+			});
+			kiosks = kiosks.concat(objects.map((obj: any, index) => {
+				return {
+					cap: capIds[i + index],
+					kiosk: kioskIds[i + index],
+					profits: obj.data?.content.fields.profits,
+					itemCount: obj.data?.content.fields.item_count,
+				}
+			}));
+		}
+
+		return kiosks;
+	}
+
+	// return the Kiosk (must be shared)
+	createKiosk(tx: TransactionBlock): TransactionResult {
+		return tx.moveCall({
+			target: `${this.packageId}::kiosk::new`,
+			arguments: [tx.object(this.multisigId)],
+		});
+	}	
+	
+	async getPolicy(nftType: string): Promise<TransferPolicy> {
+		let network: Network;
+		if (this.network == "mainnet") {
+			network = Network.MAINNET;
+		} else if (this.network == "testnet") {
+			network = Network.TESTNET;
+		} else {
+			network = Network.CUSTOM;
+		}
+		const kioskClient = new KioskClient({
+			client: this.client,
+			network,
+		});
+
+		const policies = await kioskClient.getTransferPolicies({type: nftType});
+
+		return {
+			id: policies[0].id,
+			hasFloorPrice: policies[0].rules.find(rule => rule.includes("floor_price_rule")) ? true : false,
+			hasRoyalty: policies[0].rules.find(rule => rule.includes("royalty_rule")) ? true : false,
+			isLocked: policies[0].rules.find(rule => rule.includes("kiosk_lock_rule")) ? true : false,
+		};
+	}
+
+	// 1. create a Kiosk if the Multisig doesn't have one OR
+	// 1(bis) get the Kiosk if it already exists
+	// 2. get the TransferPolicy for the type
+	// 3. transferFrom for each nft of this type
+	// 4. repeat for each type
+	// (5. share the Kiosk if it has been created in this PTB)
+	// not a proposal
+	transferFrom(
+		tx: TransactionBlock, 
+		policy: TransferPolicy,
+		multisigKiosk: string,
+		multisigCap: string,
+		senderKiosk: string,
+		senderCap: string,
+		nftId: string,
+		nftType: string,
+	): TransactionResult {
+		const [request] = tx.moveCall({
+			target: `${this.packageId}::kiosk::transfer_from`,
+			arguments: [
+				tx.object(this.multisigId), 
+				tx.object(multisigKiosk),
+				tx.object(multisigCap),
+				tx.object(senderKiosk),
+				tx.object(senderCap),
+				tx.pure(nftId),
+			],
+			typeArguments: [nftType]
+		});
+		// fill the request
+		const tpId = this.resolveRules(tx, policy, multisigKiosk, request, nftType);
+		// destroy the request
+		tx.moveCall({
+			target: `${FRAMEWORK}::transfer_policy::confirm_request`,
+			arguments: [
+				tx.object(tpId), 
+				request, 
+			],
+		});
+	}
+
+	proposeTransferTo(
+		tx: TransactionBlock,
+		key: string,
+		executionTime: number,
+		expirationEpoch: number,
+		description: string,
+		capId: string,
+		nfts: string,
+		recipient: string,
+	) {
+		tx.moveCall({
+			target: `${this.packageId}::kiosk::propose_transfer_to`,
+			arguments: [
+				tx.object(this.multisigId), 
+				tx.pure(key), 
+				tx.pure(executionTime),
+				tx.pure(expirationEpoch),
+				tx.pure(description),
+				tx.pure(capId),
+				tx.pure(nfts),
+				tx.pure(recipient),
+			],
+		});
+		this.approveProposal(tx, key);
+	}
+
+	async executeTransferTo(tx: TransactionBlock, key: string, capId: string) {
+		const ids = this.multisigData?.proposals.filter(p => p.key == key).map(p => p.action.fields.nfts);
+		if (!ids) throw new Error("Proposal is not valid");
+		const nfts = await this.client.multiGetObjects({
+			ids,
+			options: { showContent: true }
+		});
+		// TODO: get their type
+		const [action] = this.executeProposal(tx, key);
+		const [cap] = tx.moveCall({
+			target: `${this.packageId}::kiosk::borrow_cap_transfer`,
+			arguments: [action, tx.object(this.multisigId), tx.object(capId)],
+		});
+		// TODO: complete the function
+	}
+
+	private resolveRules(
+		tx: TransactionBlock, 
+		policy: TransferPolicy,
+		kiosk: string, 
+		transferRequest: any, 
+		nftType: string
+	) {
+		if (policy.hasFloorPrice) {
+			tx.moveCall({
+				target: `${this.packageId}::floor_price_rule::prove`,
+				typeArguments: [nftType],
+				arguments: [tx.object(policy.id), transferRequest],
+			});
+		}
+		if (policy.hasRoyalty) {
+			const fee = tx.splitCoins(tx.gas, [0]);
+			tx.moveCall({
+				target: `${this.packageId}::royalty_rule::pay`,
+				typeArguments: [nftType],
+				arguments: [tx.object(policy.id), transferRequest, fee],
+			});
+		} 
+		if (policy.isLocked) {
+			tx.moveCall({
+				target: `${this.packageId}::kiosk_lock_rule::prove`,
+				typeArguments: [nftType],
+				arguments: [transferRequest, tx.object(kiosk)],
+			});
+		}
+	}
 }
 
